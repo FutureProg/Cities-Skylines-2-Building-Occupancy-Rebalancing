c5f142d31b23df5152ef6b5b72f8121ab8bff03d
Nick Morrison <nickmorrison09@gmail.com>
2023-12-04 00:47:38 -0500

    Updated name of mod

--- Systems/BuildingOccupancyRebalancingSystem.cs
+++ Systems/BuildingOccupancyRebalancingSystem.cs
@@ -1,4 +1,4 @@
-using Game;
+﻿using Game;
 using Game.Audio;
 using Game.Prefabs;
 using Game.Simulation;
@@ -7,9 +7,9 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Scripting;
 
-namespace MyCoolMod.Systems
+namespace BuildingOccupancyRebalancing.Systems
 {
-    public class MyCoolModSystem : GameSystemBase
+    public class BuildingOccupancyRebalancingSystem : GameSystemBase
     {
         // private SimulationSystem simulation;
 

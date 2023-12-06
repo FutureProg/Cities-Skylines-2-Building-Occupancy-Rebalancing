a68aa432bb42d6d4e2acbdba863736f3b9d9b711
Nick Morrison <nickmorrison09@gmail.com>
2023-12-06 17:25:29 -0500

    Residential done for now, working on office towers

--- Systems/BuildingOccupancyRebalancingSystem.cs
+++ Systems/BuildingOccupancyRebalancingSystem.cs
@@ -9,6 +9,7 @@ using Unity.Burst;
 using Unity.Burst.Intrinsics;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Entities.UniversalDelegates;
 using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
@@ -108,7 +109,8 @@ namespace BuildingOccupancyRebalancing.Systems
                 buildingPropertyDataLookup = m_buildingPropertyDataLookup,
                 zoneDataLookup = m_zoneDataLookup,
                 objectGeometryLookup = m_objectGeometryLookup,
-                buildingDataLookup = m_buildingDataLookup                
+                buildingDataLookup = m_buildingDataLookup,
+                randomSeed = RandomSeed.Next()         
             };                        
             this.Dependency = job.ScheduleParallel(m_Query, this.Dependency);            
             m_endFrameBarrier.AddJobHandleForProducer(Dependency);            
@@ -129,6 +131,8 @@ namespace BuildingOccupancyRebalancing.Systems
 
             public EntityCommandBuffer.ParallelWriter commandBuffer;
 
+            public RandomSeed randomSeed;
+
             [BurstCompile]
             public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
             {   
@@ -139,63 +143,100 @@ namespace BuildingOccupancyRebalancing.Systems
                 var isOffice = chunk.Has(ref officePropertyHandle);
                 if (!isResidential) {
                     return;
-                }                                                        
+                }            
+                var random = randomSeed.GetRandom(1);                                                           
                 var prefabRefs = chunk.GetNativeArray(ref prefabRefhandle);                           
                 var underConstruction = chunk.GetNativeArray(ref underConstructionHandle);
                 for (int i = 0; i < underConstruction.Length; i++) {                        
-                    if (underConstruction[i].m_Progress < 100) continue;
-                    Plugin.Log.LogInfo($"Construction Complete! {i}");                    
+                    if (underConstruction[i].m_Progress < 100) continue;                                       
                     var prefab = underConstruction[i].m_NewPrefab;    
                     if (prefab == Entity.Null) prefab = prefabRefs[i].m_Prefab;
                     if (prefab == Entity.Null) {
-                        Plugin.Log.LogInfo("Prefab is null though...");
+                        Plugin.Log.LogInfo("Prefab is null!");
                         continue;                    
                     }
-                    if (!buildingPropertyDataLookup.TryGetComponent(prefab, out var property) ||
-                        !spawnableBuildingDataLookup.TryGetComponent(prefab, out var spawnBuildingData) ||
+                    if (!spawnableBuildingDataLookup.TryGetComponent(prefab, out var spawnBuildingData) ||
                         !buildingDataLookup.TryGetComponent(prefab, out var buildingData) ||
                         !zoneDataLookup.TryGetComponent(spawnBuildingData.m_ZonePrefab, out var zonedata)) {
-                        Plugin.Log.LogInfo("No Building Property Data");
+                        Plugin.Log.LogInfo("No Building Data");
                         continue;                        
                     }            
 
                     // Set the data
                     if(!objectGeometryLookup.TryGetComponent(prefab, out var geom)) {
-                        Plugin.Log.LogInfo("No Object Geometry");
+                        Plugin.Log.LogInfo("No Object Geometry!");
                         continue;
-                    }
-                    Plugin.Log.LogInfo($"Object Geometry: x {geom.m_Size.x}, y {geom.m_Size.y}, z {geom.m_Size.z}");
-                    Plugin.Log.LogInfo($"Building Lot Size: x {buildingData.m_LotSize.x}, y {buildingData.m_LotSize.y}");                    
-                    Plugin.Log.LogInfo($"Default Residences {property.m_ResidentialProperties}");
-                    float RESIDENTIAL_HEIGHT = 3.5f;// 3.5 Metre Floor Height for residences (looks like the vanilla height)
-                    float OFFICE_HEIGHT = 4.0f;// 4 Metre Floor Height for offices                     
-                    float FOUNDATION_HEIGHT = 1.0f; // Looks like that'd be it? Only using this for row homes                                       
-                    bool is_singleFamilyResidence = property.m_ResidentialProperties == 1; // Probably safe assumption to make until we find something else      
-                    if (is_singleFamilyResidence) {
-                        continue;
-                    }            
+                    }                                                            
                     float width = geom.m_Size.x;
                     float length = geom.m_Size.z;
-                    float height = geom.m_Size.y;
-                    bool is_RowHome = zonedata.m_ZoneFlags.HasFlag(ZoneFlags.SupportNarrow);
-                    if (is_RowHome) {                        
-                        float floorCount = (height-FOUNDATION_HEIGHT) / RESIDENTIAL_HEIGHT;                      
-                        Plugin.Log.LogInfo($"Floor count {floorCount}");                        
-                        property.m_ResidentialProperties = (int)math.floor(math.floor(floorCount) * 1.5f);// For Row Homes max 1.5 residences per floor. No basement                                                   
-                        commandBuffer.SetComponent(unfilteredChunkIndex, prefab, property);
-                    } else {
-                        var floorSize = width * length;
-                        var floorUnits = (int) math.floor(floorSize/60); // For Medium & High Density, for now, we'll assume that there's one unit per 60 square metres (600 sqft)
-                        var floorCount = (int)math.floor(height/RESIDENTIAL_HEIGHT); 
-                        property.m_ResidentialProperties = floorUnits * floorCount;
-                    }                                               
-                    Plugin.Log.LogInfo("Building Data Level: " + spawnBuildingData.m_Level);                    
-                    Plugin.Log.LogInfo("Zone AreaType " + zonedata.m_AreaType);
-                    Plugin.Log.LogInfo("Zone MaxHeight " + zonedata.m_MaxHeight);
-                    Plugin.Log.LogInfo("Zone ZoneFlags " + zonedata.m_ZoneFlags.ToString());
-                    Plugin.Log.LogInfo("Zone ZoneType Index" + zonedata.m_ZoneType.m_Index);                    
-                    Plugin.Log.LogInfo("====");
+                    float height = geom.m_Size.y;                    
+                    if (isResidential) {
+                        UpdateResidential(unfilteredChunkIndex, width, length, height, random, zonedata, prefab);
+                    }
+                    else if (isOffice) {
+                        float OFFICE_HEIGHT = 4.0f;// 4 Metre Floor Height for offices      
+                        float SPACE_PER_EMPLOYEE = 15; // 15 metres needed per employee
+                        float COMPANIES_PER_FLOOR = 0.5f;
+                        int floorCount = (int)math.floor(height / OFFICE_HEIGHT);
+                    }
+                    
+                    // Plugin.Log.LogInfo($"Object Geometry: x {geom.m_Size.x}, y {geom.m_Size.y}, z {geom.m_Size.z}");
+                    // Plugin.Log.LogInfo($"Building Lot Size: x {buildingData.m_LotSize.x}, y {buildingData.m_LotSize.y}");                                        
+
+                    // Plugin.Log.LogInfo("Building Data Level: " + spawnBuildingData.m_Level);                    
+                    // Plugin.Log.LogInfo("Zone AreaType " + zonedata.m_AreaType);
+                    // Plugin.Log.LogInfo("Zone MaxHeight " + zonedata.m_MaxHeight);
+                    // Plugin.Log.LogInfo("Zone ZoneFlags " + zonedata.m_ZoneFlags.ToString());
+                    // Plugin.Log.LogInfo("Zone ZoneType Index" + zonedata.m_ZoneType.m_Index);                    
+                    // Plugin.Log.LogInfo("====");
+                }                                  
+            }
+
+            void UpdateResidential(int unfilteredChunkIndex, float width, float length, float height, Unity.Mathematics.Random random, 
+                    ZoneData zonedata, Entity prefab) {
+                if (!buildingPropertyDataLookup.TryGetComponent(prefab, out var property)) {
+                    return;
                 }
+                bool is_singleFamilyResidence = property.m_ResidentialProperties == 1; // Probably safe assumption to make until we find something else                                      
+                if (is_singleFamilyResidence) {
+                    // Plugin.Log.LogInfo("Skipping Single Family Residential\n=======");
+                    return;
+                }
+                // Plugin.Log.LogInfo($"Default Residences {property.m_ResidentialProperties}");
+                float RESIDENTIAL_HEIGHT = 3.5f;// 3.5 Metre Floor Height for residences (looks like the vanilla height)                                   
+                float FOUNDATION_HEIGHT = 1.0f; // Looks like that'd be it? Only using this for row homes    
+                float MIN_RESIDENCE_SIZE = 80;                    
+                float MAX_RESIDENCE_SIZE = 111; // between 800sqft and 1200sqft  (80sqm and 111sqm)  
+
+                bool is_RowHome = zonedata.m_ZoneFlags.HasFlag(ZoneFlags.SupportNarrow);
+                if (is_RowHome) {                        
+                    float floorCount = (height-FOUNDATION_HEIGHT) / RESIDENTIAL_HEIGHT;                      
+                    // Plugin.Log.LogInfo($"Floor count {floorCount}");                        
+                    property.m_ResidentialProperties = (int)math.floor(math.floor(floorCount) * 1.5f);// For Row Homes max 1.5 residences per floor. No basement                                                                           
+                } else {
+                    var floorSize = width * length;
+                    // Plugin.Log.LogInfo($"Floor Size: {floorSize}");
+                    int floorUnits = 0;
+                    if (height < 52) { // Ignore mid-rise buildings, they're usually fine
+                        // Plugin.Log.LogInfo("Midrise, ignoring");
+                        // Plugin.Log.LogInfo("===");
+                        return;
+                    }
+                    while(floorSize > MIN_RESIDENCE_SIZE+2) {                                                   
+                        float maximum = floorSize < MAX_RESIDENCE_SIZE? floorSize : MAX_RESIDENCE_SIZE;                            
+                        float minimum = MIN_RESIDENCE_SIZE;                            
+                        floorSize -= random.NextFloat(minimum, maximum);
+                        floorUnits++;
+                    }                        
+                    // Plugin.Log.LogInfo($"Floor Units: {floorUnits}");
+                    var floorCount = (int)math.floor(height/RESIDENTIAL_HEIGHT); 
+                    // Plugin.Log.LogInfo($"Floor count {floorCount}");                           
+                    floorCount -= 1; // Remove for the lobby floor 
+                    property.m_ResidentialProperties = floorUnits * floorCount;
+                    // Plugin.Log.LogInfo($"New Residential Property Count: {floorUnits * floorCount}");                                     
+                } 
+                commandBuffer.SetComponent(unfilteredChunkIndex, prefab, property); 
+                // Plugin.Log.LogInfo($"New Residential Property Count: {property.m_ResidentialProperties}");      
             }
         }
 
